c0a54b09df0ccac261ae57936a41c3cb91edadad
HBhawe <harshalb97@outlook.com>
2024-11-14 11:26:14 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+})
+
+app.get("/about", (req, res) => {
+    res.render("about.ejs");
+})
+
+app.get("/contact", (req, res) => {
+    res.render("contact.ejs");
+})
+
+app.get("/blog", (req, res) => {
+    res.render("blog.ejs");
+})
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+})
+
+const blog = [];
